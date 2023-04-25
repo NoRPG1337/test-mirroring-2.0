c25562c5a1ba3ae2464521bdd82b9eac0d9ad0e7
Dmitry Abramov <d.abramov@haulmont.com>
2023-04-25 18:43:17 +0000

    Update ServiceConfiguration.java

--- modules/core/src/main/java/com/haulmont/shamrock/test/mirroring/2/0/ServiceConfiguration.java
+++ modules/core/src/main/java/com/haulmont/shamrock/test/mirroring/2/0/ServiceConfiguration.java
@@ -4,6 +4,8 @@ import com.haulmont.monaco.config.annotations.Config;
 import com.haulmont.monaco.config.annotations.Property;
 import org.picocontainer.annotations.Component;
 
+// tests
+
 
 @Config
 @Component
